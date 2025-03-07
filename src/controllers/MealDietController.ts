9a8ddb31be5729495868af67a0281df9c1dd3b6f
Samuel <colaco.samuel8@gmail.com>
2025-03-07 09:09:16 -0300

    Criando rota de listar refeições

--- src/controllers/MealDietController.ts
+++ src/controllers/MealDietController.ts
@@ -62,4 +62,66 @@ export class MealDietController{
 
         res.status(200).send()
     }
+
+    async indexAllMealFromUser(req: FastifyRequest, res: FastifyReply){
+
+        const paraSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = paraSchema.parse(req.params)
+
+        const userExist = await prisma.user.findFirst({ where: { id }})
+
+        if(!userExist){
+            throw new AppError("Usuário não existe")
+        }
+
+        const mealsExist = await prisma.dietMeal.findMany({ where: { userId: id }})
+
+        if(mealsExist.length === 0){
+            throw new AppError("Esse usuário não tem refeições cadastradas")
+        }
+
+        res.status(200).send({ meals: mealsExist})
+
+    }
+
+    async numberOfMealsRegisterByUser(req: FastifyRequest, res: FastifyReply){
+
+        const paramSchema = z.object({
+            id: z.string().uuid()
+        })
+
+        const { id } = paramSchema.parse(req.params)
+
+        const userExist = await prisma.user.findFirst({ where: { id }})
+
+        if(!userExist){
+            throw new AppError("Usuário não existe")
+        }
+
+        const mealsExist = await prisma.dietMeal.findMany({ where: { userId: id }})
+
+        if(mealsExist.length === 0){
+            throw new AppError("Esse usuário não tem refeições cadastradas")
+        }
+        
+        let inDietMeals = 0
+
+        let notInDietMeals = 0
+
+        mealsExist.forEach((meal) => {
+            if(meal.InDiet === true){
+                inDietMeals =+ 1
+            }
+            else{
+                notInDietMeals =+ 1
+            }
+        })
+
+        res.status(200).send({ numberOfMealsRegister: mealsExist.length, numberOfMealsInDiet: inDietMeals, numberOfMealsNotInDiet: notInDietMeals})
+
+
+    }
 }
--- src/routes/mealRoutes.ts
+++ src/routes/mealRoutes.ts
@@ -12,4 +12,6 @@ export function mealRoutes(mealRoutes: FastifyInstance){
     mealRoutes.get("/meal", { preHandler : [ Autenticated, VerifyUserAuthorization(["admin"])]},mealDietController.index)
     mealRoutes.post("/meal", { preHandler: [Autenticated, VerifyUserAuthorization(["member", "admin"])]}, mealDietController.create)
     mealRoutes.put("/meal/:id", { preHandler : [Autenticated, VerifyUserAuthorization(["admin"])]}, mealDietController.update)
+    mealRoutes.get("/meal/:id", { preHandler: [Autenticated, VerifyUserAuthorization(["admin"])]}, mealDietController.indexAllMealFromUser)
+    mealRoutes.get("/meal/list/:id", { preHandler: [Autenticated, VerifyUserAuthorization(["admin"])]}, mealDietController.numberOfMealsRegisterByUser)
 }
